741cd7fbb587a62160310f565ddff0f9d72d4cb6
Alexander Rankov <alexanderrankov@gmail.com>
2025-11-30 14:20:20 -0500

    I'm adding the initial elements for my bank project. I added the main text and made it blue

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -7,18 +7,32 @@ public class MainFrame extends JFrame implements Runnable {
 
     JPanel mainPanel = new JPanel();
 
+    JLabel mainText;
+
     @Override
     public void run()
     {
         configurateWindow();
+        configurateDesign();
     }
 
     public void configurateWindow()
     {
         this.setSize(new Dimension(1920,1080));
+        this.setExtendedState(MAXIMIZED_BOTH);
         this.setTitle("Bank of Rankov");
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
+    }
+
+    public void configurateDesign()
+    {
+        mainPanel.setLayout(new BorderLayout());
+
+        mainText = new JLabel("Bank of Rankov");
+        mainText.setForeground(new Color(0x0077ff));
+
+        add(mainText);
     }
 
 }
